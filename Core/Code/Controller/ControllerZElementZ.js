6017bfd28d83d48f1aa933a3391403a9be72de57
zeecase <mszeecase@gmail.com>
2025-08-24 13:17:02 -0700

    Add guide states

--- Core/Code/Controller/ControllerZElementZ.js
+++ Core/Code/Controller/ControllerZElementZ.js
@@ -22,9 +22,8 @@ export default class ControllerZElementZ extends ControllerZ{
             light: "#E7E7E7", medium: "#373a38", dark: "#131313",
             red: "#ab595a", orange: "#b38945", yellow: "#b6a243",
             green: "#7e8750", blue: "#3c70a5", purple: "#8348a5",
-            theme: "#b38945"
         };
-        this.colors.theme = this.colors.orange;
+        this.theme = "orange";
         this.darkMode = false;
 
         this.active = "timeZ";
@@ -98,7 +97,7 @@ export default class ControllerZElementZ extends ControllerZ{
     getGuide(){
         for(let x=0; x<this.elementZList.length; x++){
             let guide = this.elementZList[x];
-            if(guide.id == "guideZ")
+            if(guide.id == "FuZ")
                 return guide.getView();
         }
     }
--- Core/Code/Element/Guide/GuideZ.js
+++ Core/Code/Element/Guide/GuideZ.js
@@ -11,22 +11,65 @@ const emotes = ["away", "focus", "rest", "left", "right", "up", "down", "tense",
 export default class GuideZ extends ElementZ{
 
     constructor(id) {
+        id = "FuZ";
         super(id);
 
-        this.emote = "away";
+        this.emote = "rest";
         this.guide = document.createElement("div");
         this.guideBody = document.createElement("img");
         this.guideEyeL = document.createElement("img");
         this.guideEyeR = document.createElement("img");
         this.guideAccessory = document.createElement("img");
+
+        this.guide.appendChild(this.guideBody);
+        this.guide.appendChild(this.guideEyeL);
+        this.guide.appendChild(this.guideEyeR);
+        this.guide.appendChild(this.guideAccessory);
     }
 
     start(){
-
+        this.setGuide(this.emote);
     }
 
     update(){
 
+    }
+
+    setGuide(emote){
+        this.emote = emote;
+        this.guideBody.src = guideFolder + "Body/" + this.id + "_" + this.elementController.theme + ".png";
+
+        if(emote == "focus"){
+            this.guideEyeL.src = guideFolder + "Eye/Left/" + this.id + "_open.png";
+            this.guideEyeR.src = guideFolder + "Eye/Right/" + this.id + "_open.png";
+            this.guideAccessory.src = guideFolder + "Accessory/" + this.id + "_high.png";
+        } else if(emote == "rest"){
+            this.guideEyeL.src = guideFolder + "Eye/Left/" + this.id + "_closed.png";
+            this.guideEyeR.src = guideFolder + "Eye/Right/" + this.id + "_closed.png";
+            this.guideAccessory.src = guideFolder + "Accessory/" + this.id + "_rest.png";
+        } else if(emote == "left"){
+
+        } else if(emote == "right"){
+
+        } else if(emote == "up"){
+
+        } else if(emote == "down"){
+
+        } else if(emote == "tense"){
+
+        } else if(emote == "sus"){
+
+        } else if(emote == "worry"){
+
+        } else if(emote == "blink"){
+
+        } else if(emote == "question"){
+
+        } else if(emote == "happy"){
+
+        } else if(emote == "super"){
+
+        }
     }
 
     getView(){
@@ -41,16 +84,38 @@ export default class GuideZ extends ElementZ{
             this.guide.style.width = "100px";
             this.guide.style.setProperty("border-radius", "50%");
 
-            this.guideImg.src = "/Core/Asset/Image/Guide/FuZ/FuZ_"+this.state+".png";
-            this.guideImg.style.setProperty("position", "fixed");
-            this.guideImg.style.setProperty("top", "5%");
-            this.guideImg.style.setProperty("left", "50%");
-            this.guideImg.style.setProperty("-webkit-transform", "translate(-50%, 0)");
-            this.guideImg.style.setProperty("transform", "translate(-50%, 0)");
-            this.guideImg.style.width = "90%";
-            this.guideImg.style.height = "90%";
+            this.guideBody.style.setProperty("position", "fixed");
+            this.guideBody.style.setProperty("top", "5%");
+            this.guideBody.style.setProperty("left", "50%");
+            this.guideBody.style.setProperty("-webkit-transform", "translate(-50%, 0)");
+            this.guideBody.style.setProperty("transform", "translate(-50%, 0)");
+            this.guideBody.style.width = "90%";
+            this.guideBody.style.height = "90%";
 
-            this.guide.appendChild(this.guideImg);
+            this.guideEyeL.style.setProperty("position", "fixed");
+            this.guideEyeL.style.setProperty("top", "5%");
+            this.guideEyeL.style.setProperty("left", "50%");
+            this.guideEyeL.style.setProperty("-webkit-transform", "translate(-50%, 0)");
+            this.guideEyeL.style.setProperty("transform", "translate(-50%, 0)");
+            this.guideEyeL.style.width = "90%";
+            this.guideEyeL.style.height = "90%";
+
+            this.guideEyeR.style.setProperty("position", "fixed");
+            this.guideEyeR.style.setProperty("top", "5%");
+            this.guideEyeR.style.setProperty("left", "50%");
+            this.guideEyeR.style.setProperty("-webkit-transform", "translate(-50%, 0)");
+            this.guideEyeR.style.setProperty("transform", "translate(-50%, 0)");
+            this.guideEyeR.style.width = "90%";
+            this.guideEyeR.style.height = "90%";
+
+            this.guideAccessory.style.setProperty("position", "fixed");
+            this.guideAccessory.style.setProperty("top", "5%");
+            this.guideAccessory.style.setProperty("left", "50%");
+            this.guideAccessory.style.setProperty("-webkit-transform", "translate(-50%, 0)");
+            this.guideAccessory.style.setProperty("transform", "translate(-50%, 0)");
+            this.guideAccessory.style.width = "90%";
+            this.guideAccessory.style.height = "90%";
+
         }
 
         return this.guide;
